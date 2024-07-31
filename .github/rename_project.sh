bbcc369895829cc5d26a6d4aa2884c67f0e72e0d
ljhowell <ljhowell@users.noreply.github.com>
2024-07-31 16:48:35 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="ljhowell"
+original_name="radex"
+original_urlname="radex"
+original_description="Awesome radex created by ljhowell"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv radex $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
